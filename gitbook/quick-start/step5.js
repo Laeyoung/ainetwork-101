e472e45b84368857fbc5a936b451aa8e2046f23e
Laeyoung <gadise@gmail.com>
2022-08-30 13:48:05 +0000

    update step5.js and update-gitbook.md

--- gitbook/quick-start/step5.js
+++ gitbook/quick-start/step5.js
@@ -1,31 +1,26 @@
 const Ain = require('@ainblockchain/ain-js').default;
 const ain = new Ain('https://testnet-api.ainetwork.ai', 0);
 
-// Copy and paste from output of step2-3.js
-const myAddress = {
-  address: '0x85',
-  private_key: '79',
-  public_key: 'b6d'
-}
+// Copy and paste private_key from step2-3.js
+const private_key = 'COPY-AND-PASTE-PRIVATE-KEY';
 
-// Set the new account (myAddress) as the default account
-ain.wallet.setDefaultAccount(myAddress);
+const myAddress = ain.wallet.addAndSetDefaultAccount(private_key);
 
-// Print defaultAccount (Need to backup your private key)
-console.log(ain.wallet.defaultAccount);
-// Example:
-// {
-//   address: '0x09A0d53FDf1c36A131938eb379b98910e55EEfe1',
-//   private_key: '...',
-//   public_key: '...'
-// }
+console.log(myAddress);
 
-// Transfer 10 AIN to ADDRESS_TO_SEND_AIN_TO
-ain.wallet.transfer({
-  from: myAddress, // Optional (defaultAccount will be used if omitted)
-  to: ADDRESS_TO_SEND_AIN_TO, // e.g. 0x08Aed7AF9354435c38d52143EE50ac839D20696b
-  value: 10
-})
+/**
+ * Transfer 0.1 AIN to 0x99bBa0051DDdf7b69972602512661915fdD8eE89
+ *
+ * And then,
+ * check your transaction at https://testnet-insight.ainetwork.ai/transactions/${YOUR-TX-HASH},
+ * or check receiver's address at https://testnet-insight.ainetwork.ai/accounts/0x99bBa0051DDdf7b69972602512661915fdD8eE89
+ */
+ain.wallet
+  .transfer({
+    from: myAddress, // Optional (defaultAccount will be used if omitted)
+    to: '0x99bBa0051DDdf7b69972602512661915fdD8eE89',
+    value: 0.1,
+  })
   .then((res) => {
     // The returned value will be either
     // { result, tx_hash } or { code, message, tx_hash },
--- gitbook/quick-start/update-gitbook.md
+++ gitbook/quick-start/update-gitbook.md
@@ -1,9 +1,25 @@
 # Edit 및 Update 필요
 
-https://docs.ainetwork.ai/ain-blockchain/developer-guide/getting-started#step-4.-get-ain-for-free
+### [Step4](https://docs.ainetwork.ai/ain-blockchain/developer-guide/getting-started#step-4.-get-ain-for-free)
+
 1. https 주소로 바꿔야함
 2. 의견1) `We have a faucet site that gives you 100 AIN per day (valid only on the Testnet)` 보다 Testnet에 AIN이 들어간다고 말하는게 맞지 않으려나?
 3. 의견2) faucet 주소도 testnet 이 명시 되는게 맞지 않으려나?
 
+### [Step5](https://docs.ainetwork.ai/ain-blockchain/developer-guide/getting-started#step-5.-transfer-ain)
 
+1. Step 3에서 만든 private key를 안 쓰고 새로 생성함. 그러면 step 4를 또 다시 해야함. 하지만 벌써 코드가 다 실행되어서 순서상 안 맞음.
+2. `ADDRESS_TO_SEND_AIN_TO` 를 넣는게 무척 막막함. 여기서는 그냥 address를 알려주는게 맞을 것 같음. (괜찮으면 이거 쓰세요 0x99bBa0051DDdf7b69972602512661915fdD8eE89). 저 sample address에 쌓인 AIN이나 TX로 Quick Start를 몇명이나 진행 했는지 간접적으로 알 수 있음. Testnet이 reset 되기 전까지는.
+3. Faucet은 1 AIN을 주는데, step 5에서는 10 AIN을 보내라고 함. 0.1 AIN 정도로 바꾸면 좋을 듯
 
+### [Step6](https://docs.ainetwork.ai/ain-blockchain/developer-guide/getting-started#step-6.-optional-setting-the-nonce-of-your-transaction)
+
+1. 이 내용이 필요한가? Quick Start에? 이걸 쓰는 경우가 있는가?
+
+### [Step7](https://docs.ainetwork.ai/ain-blockchain/developer-guide/getting-started#step-7.-create-your-own-app)
+
+1. 이 내용이 필요한가? Quick Start에? 이걸 쓰는 경우가 있는가?
+
+### 전체 내용
+
+- Quick Start인데 너무 내용이 많지 않은가? Section이라도 나뉘어 있으면 어떨까요? 현재 구조상 Step 1 ~ 5 (일반적으로 블록체인에서는 다 있는 코인 보내기), Step 7 ~ 10 정도 (AI Network 특징)?
